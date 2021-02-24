dfc7addba61139c8e3f9b2be2d55802aaf10a976
abhishek sappandi <abhisheksappandi@gmail.com>
2021-02-24 20:47:32 +0530

    small changes

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const ConfigDB = () =>{
-    mongoose.connect(`mongodb+srv://abhi:abhi@database.lze2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{useUnifiedTopology:true,useNewUrlParser:true})
+    mongoose.connect(`mongodb+srv://abhi:<abhi>@database.lze2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{useUnifiedTopology:true,useNewUrlParser:true})
     .then(()=>{
         console.log("connected to DB")
     })
